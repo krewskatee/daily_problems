bc967c00381e7d3c5727e11dcf41ce070004ef41
krewskatee <lchira99@gmail.com>
2017-08-24 11:33:36 -0500

    fixed comment typo

--- week_1/thursday_8-24-17.rb
+++ week_1/thursday_8-24-17.rb
@@ -1,5 +1,5 @@
 def find_longest_word(string)
-  string.split(/[^\w]+/).max_by(&:length) #regular expression to split spaces and all non-wrong characters
+  string.split(/[^\w]+/).max_by(&:length) #regular expression to split spaces and all non-word characters
 end
 
 p find_longest_word("Are you lame or supercalifragilisticexpialidocious?")
