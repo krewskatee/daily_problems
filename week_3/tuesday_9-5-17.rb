1b09a69769a12f20ef96c0af06837eba65e79f7b
krewskatee <lchira99@gmail.com>
2017-09-05 23:33:21 -0500

    added daily problem

---
+++ week_3/tuesday_9-5-17.rb
@@ -0,0 +1,9 @@
+items = [
+  {title: "NoDBA", words: 561, tags: ["nosql", "people", "orm"], type: :book},
+  {title: "Infodeck", words: 1145, tags: ["nosql", "writing"], type: :book},
+  {title: "OrmHate", words: 1718, tags: ["nosql", "orm"], type: :book},
+  {title: "ruby", words: 1313, tags: ["ruby"], type: :article},
+  {title: "DDD_Aggregate", words: 482, tags: ["nosql", "ddd"], type: :book}
+]
+
+p items.select { |k| k[:type] == :book }.map{|x| x[:words]}.reduce(:+)
