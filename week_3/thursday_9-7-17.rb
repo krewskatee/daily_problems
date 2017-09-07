8becf3b2fa44d0b556a3a86ad4c27dc7cffc1d71
krewskatee <lchira99@gmail.com>
2017-09-07 10:02:20 -0500

    made changes for array input

--- week_3/thursday_9-7-17.rb
+++ week_3/thursday_9-7-17.rb
@@ -1,10 +1,11 @@
-def sum_of_range(starting, ending)
-  starting, ending = ending, starting if ending < starting
+def sum_of_range(arr)
+  arr[0], arr[1] = arr[1], arr[0] if arr[1] < arr[0]
   sum = 0
-  (starting..ending).each do |n|
+  (arr[0]..arr[1]).each do |n|
     sum += n
   end
   sum
 end
 
-p sum_of_range(1, 4)
+p sum_of_range([1, 4])
+p sum_of_range([4, 1])
