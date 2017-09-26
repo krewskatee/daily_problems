c43351941a2677564be65be1431441339ea2c473
krewskatee <lchira99@gmail.com>
2017-09-26 18:23:09 -0500

    added week6 monday and tuesday

--- extra/python.py
+++ extra/python.py
@@ -1,23 +1,34 @@
+def decimal_to_binary(decimal_number):
+    x=decimal_number
+    binary_array=[]
+    while (decimal_number>0):
+        a=int(float(decimal_number%2))
+        binary_array.append(a)
+        decimal_number=(decimal_number-a)/2
+    binary_array.append(0)
+    string=""
+    for j in binary_array[::-1]:
+        string=string+str(j)
+    print('The binary no. for %d is %s'%(x, string))
+
+def binary_to_decimal(binary_number):
+
+    binary_list = list(binary_number)
+    binary_length = len(binary_list)
+
+    index = 0
+    power = binary_length - 1
+    total = 0
+
+    while(power >= 0):
+        total += (int(binary_list[index])) * (2**(power))
+        index += 1
+        power -= 1
+    print(total)
 
 
-# Problem 4 (15 points):
 
-country1 = "the-united-states-of-america"
-country2 = "the People's Republic of China"
-country3 = "jAPAN"
-country4 = "Italian Republic"
-country5 = "Great Socialist People's Libyan Arab Jamahiriya"
-
-list1 = []
-list1.append(country1.split('-')[1][0].upper())
-list1.append(country1.split('-')[2][0].upper())
-list1.append(country1.split('-')[4][0].upper())
-list1 = '.'.join(list1)
-print(list1)
-
-list1 = []
-list1.append(country2.split(' ')[1][0].upper())
-list1.append(country2.split(' ')[2][0].upper())
-list1.append(country2.split(' ')[4])
-list1 = '.'.join(list1)
-print(list1)
+decimal_number = int(input("Please enter a decimal number: "))
+decimal_to_binary(decimal_number)
+binary_number = input("Please enter binary number: ")
+binary_to_decimal(binary_number)
---
+++ week_6/monday_9-25-17.rb
@@ -0,0 +1,3 @@
+sentence = "You can live like a king but make sure it isn't a lie."
+
+p sentence.scan(/\b[l]\w{3}\b/)
